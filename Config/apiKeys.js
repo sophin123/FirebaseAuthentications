3e566d1a5e1acfdf0c0d4940e90c2a07eff77d2a
sophin123 <46114283+sophin123@users.noreply.github.com>
2021-06-24 13:20:18 +1000

    Update apiKeys.js

--- Config/apiKeys.js
+++ Config/apiKeys.js
@@ -1,14 +1,14 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCozl46fE3r9sGz7Hc5EFSo8NjASVid5tk",
-  authDomain: "projectjs-5ed0c.firebaseapp.com",
-  databaseURL: "https://projectjs-5ed0c-default-rtdb.firebaseio.com",
-  projectId: "projectjs-5ed0c",
+  apiKey: "Your API KEY",
+  authDomain: "Your authorization domain ID",
+  databaseURL: "Your Database URL locations ",
+  projectId: "Your Project ID",
   storageBucket: "projectjs-5ed0c.appspot.com",
-  messagingSenderId: "630178655347",
-  appId: "1:630178655347:web:47326fa6c858dc1cfc0e06",
-  measurementId: "G-QLWNC64PHS",
+  messagingSenderId: "Sender ID",
+  appId: "App ID",
+  measurementId: "Measurement ID",
 };
 
 firebase.initializeApp(firebaseConfig);
